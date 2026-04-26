ee248577aaaf68f285548dd86ecc209f488b6fb1
Harsh Pratap Singh <rudrapratap4519@gmail.com>
2026-04-26 16:37:22 +0530

    custom api response

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import connectDB from "./db/index.js";
 dotenv.config({path:'./env'})
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+
+        console.log("ERR",error);
+        throw error
+        
+    })
+
+
+    app.listen(process.env.PORT||8000,()=>{
+
+
+        console.log(`app is listening at port:${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+
+    console.log("mongodb conection failed:",err)
+})
 
 
 // (async()=>{
